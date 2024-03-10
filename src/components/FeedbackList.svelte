7346bdf22f54406d5c0a6893f35ede92d250f2e3
sudip <sudipgurungworks@gmail.com>
2024-03-10 14:38:04 +0100

    fiorst and last commit

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { FeedbackStore } from "./../stores.js";
+  import FeedbackItem from "./FeedbackItem.svelte";
+  import { fade, scale } from "svelte/transition";
+
+</script>
+
+<main>
+  {#each $FeedbackStore as fb (fb.id)}
+    <!-- content here -->
+    <div in:scale out:fade={{ duration: 100 }}>
+      <FeedbackItem item={fb}/>
+    </div>
+  {/each}
+</main>
